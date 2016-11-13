656f1ecd259c42c8e06f6d13c2b803133f007d53
Timothy Voice <timsethvoice@gmail.com>
2016-11-13 09:23:56 -0500

    added function for Google Civic API

--- src/data/connectors.js
+++ src/data/connectors.js
@@ -11,15 +11,28 @@ const googleUrl = `https://www.googleapis.com/civicinfo/v2/representatives`;
 // Google Civic
 
 module.exports.reps = {
+  /**
+   * Get the Reps for a zip code
+   * @param {string} zip_code Zip code to search for
+   **/
   get(zip_code) {
     return new Promise((resolve, reject) => {
       rp(`${googleUrl}?address=${zip_code}&key=${GOOGLE_CIVIC_KEY}`)
         .then((res) => JSON.parse(res))
         .then((res) => {
-          // Parse reps
+          // offices[{index:x}]
+          // officesrs[x]
+          res.offices = res.offices.map((office, idx, arr) => {
+            office.officials = [];
+            office.officialIndices.forEach((index) => {
+              office.officials.push(res.officials[index]);
+            })
+            return office;
+          })
+          resolve(res);
         })
         .catch((err) => { throw err})
-    })
+    });
   }
 }
 
---
+++ src/test/google.civic.test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import casual from 'casual';
+
+import { reps } from '../data/connectors.js';
+
+chai.use(chaiAsPromised);
+const assert = chai.assert;
+
+describe('Google API Data Cleaning', function () {
+  it('should return an object with the Offices property', function () {
+    return assert.eventually.property(reps.get('01267'), 'offices');
+  })
+  it('should have data with the officers info in the office', function () {
+    return assert.eventually.deepProperty(reps.get('11218'), 'offices[0].officials');
+  })
+  it('should have the first office equal the president', function () {
+    return assert.eventually.deepPropertyVal(reps.get('01267'), 'offices[0].officials[0].name', 'Barack Obama');
+  })
+})
