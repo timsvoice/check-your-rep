3a56230d37c9686b49230fec6c42ab6aa02203df
Timothy Voice <timsethvoice@gmail.com>
2016-11-16 11:40:15 -0500

    added webpack and yarn and modified apollo queries

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,40 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
     uri: 'http://checkyourrep-api-env.us-west-2.elasticbeanstalk.com/'
   })
 })
+
+const Bills = ({ params, data, state }) => {
+  console.log(data);
+  return (
+    <div>
+      { !data.loading ? <h1>Data { data.bills[0].title }</h1> : <div>Loading!</div> }
+    </div>
+  );
+};
+
+const BillsData =  gql`
+  {
+    bills(chamber:"house", type:"introduced") {
+      title
+      summary
+    }
+  }`;
+
+const BillsWithData = graphql(BillsData)(Bills);
+
+const App = ({ children, params, location }) => (
+  <div className="container">
+    <BillsWithData />
+  </div>
+);
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>),
+  document.getElementById('App')
+)
