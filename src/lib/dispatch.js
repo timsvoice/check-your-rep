8d24c15bb055844fd6bcc33daa087ac9c8902b57
Timothy Voice <timsethvoice@gmail.com>
2016-11-16 13:12:41 -0500

    fixed index call of dispatch

--- src/lib/dispatch.js
+++ src/lib/dispatch.js
@@ -42,8 +42,8 @@ module.exports.dispatch = {
     // schedule the function to scan bills everyday at 6PM
     const rule = new schedule.RecurrenceRule();
     rule.dayOfWeek = [1,2,3,4,5];
-    rule.hour = 11;
-    rule.minute = 15;
+    rule.hour = 13;
+    rule.minute = 30;
 
     return new Promise((resolve, reject) => {
       schedule.scheduleJob(rule, () => {
