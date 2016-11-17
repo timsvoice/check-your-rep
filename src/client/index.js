069491912b2aa09cc33e7f8d636acee477a6ff26
Timothy Voice <timsethvoice@gmail.com>
2016-11-17 12:14:32 -0500

    Added webpack-dev-server and Material UI (Deps)

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// const client = new ApolloClient();
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
